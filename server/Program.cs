ff3e95711e014c79659ef51748e48cd839a3f778
Editor Jakupi <da.editoor@msn.com>
2025-02-07 10:00:03 +0100

    Initilized the database connection

--- server/Program.cs
+++ server/Program.cs
@@ -1,6 +1,35 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using DotNetEnv;
+using app.Database;
+using Microsoft.AspNetCore.Http;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Builder;
 
-app.MapGet("/", () => "Hello World!");
 
-app.Run();
+
+
+
+
+class Program
+{
+    static async Task Main()
+    {
+
+        Env.TraversePath().Load();//laddar in variabler från .env filen behöver en variabel exempel nedaför när vi har skapat vår db
+        // DBConnectString="Host=localhost;Port=5432;Username=postgres;Password=pass123;Database=dbname;SearchPath=public"        
+        DatabaseConnection database = new();
+
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+
+
+
+
+
+        app.MapGet("/", () => "Hello World!");
+
+        app.Run();
+
+
+    }
+}
