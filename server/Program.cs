419f1c1467a2c31666126007233189401cd107b1
Editor Jakupi <da.editoor@msn.com>
2025-02-11 14:13:13 +0100

    Removed DotNetEnv. added login.txt in gitignore and some small modifications

--- server/Program.cs
+++ server/Program.cs
@@ -1,5 +1,3 @@
-using DotNetEnv;
-using app.Database;
 using Microsoft.AspNetCore.Http;
 using System.Security.Cryptography;
 using Microsoft.AspNetCore.Builder;
@@ -11,8 +9,8 @@ class Program
     static async Task Main()
     {
 
-        //Env.TraversePath().Load();//laddar in variabler från .env filen behöver en variabel exempel nedaför när vi har skapat vår db
-        // DBConnectString="Host=localhost;Port=5432;Username=postgres;Password=pass123;Database=dbname;SearchPath=public"        
+        //var variableList = FileReader.Load("logIn.txt"); //laddar in variabler från .login.txt filen behöver en variabel exempel nedaför när vi har skapat vår db
+        // DBConnectString=Host=localhost    Port = 5432  Username = postgre Password = databas123 Database = dissatisfiedcustommer SearchPath =public
 
         var variableList = FileReader.Load("logIn.txt");
 
