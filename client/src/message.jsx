5952034d4c7b7afd053fc19f3a9716b28b73c5f1
villiamxd <villiam.strandh45@gmail.com>
2025-02-12 11:24:10 +0100

    submitMessage function & minor styling changes
    submitMessage function called when clicking submit button and minor
    detail changes on buttons.

--- client/src/message.jsx
+++ client/src/message.jsx
@@ -5,10 +5,31 @@ import './message.css'
 export function Message() {
   return <main>
     <ul>
-      <div><h3>Title</h3><input /></div>
-      <div><h3>Email</h3><input /></div>
-      <div><h3>Message</h3><input /></div>
-      <div><button class="cancel button">Cancel</button><button class="submit button">Submit</button></div>
+      <div><h3>Title</h3><input class="title" placeholder='Title for your problem'/></div>
+      <div><h3>Email</h3><input class="email" placeholder='example@mail.com'/></div>
+      <div><h3>Message</h3><textarea class="message" placeholder='Write your problem in detail here'/></div>
+      <div><button class="cancel button">Cancel</button><button class="submit button" onClick={submitMessage}>Submit</button></div>
     </ul>
   </main>
+}
+
+function submitMessage(){
+
+
+  fetch("/api/messages", {
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    method: "POST",
+    body: JSON.stringify({
+      data: {
+        Email : document.getElementsByClassName("email").value,
+        Name : document.getElementsByClassName("title").value,
+        Content : document.getElementsByClassName("message").value
+      }
+    })
+    }).then(response => {
+      console.log(response);
+  });
 }
