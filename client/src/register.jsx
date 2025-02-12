72e48f852c619f6aaa05a91b948dbb4743c7bb24
Marchant01 <celvin.marchant@hotmail.com>
2025-02-12 15:48:48 +0100

    Implemented register page
    Implemented a page where a new user can register. Functions needed.

---
+++ client/src/register.jsx
@@ -0,0 +1,30 @@
+import './register.css'
+
+export default function RegisterMain()
+{
+  return <main>
+    <div id="input-div"> 
+      <p>Email-address:</p>
+      <label>
+        <input type='text' name='email-address-input' placeholder='example@mail.com' />
+      </label>
+      <p>Phone-number:</p>
+      <label>
+        <input type='text' name='phone-number-input' placeholder='076000000' />
+      </label>
+      <p>Password:</p>
+      <label>
+        <input type='Password' name='password-input' placeholder='Password' />
+      </label>
+      <p>Confirm Password:</p>
+      <label>
+        <input type='Password' name='confirm-password-input' placeholder='Confirm Password' />
+      </label>
+    </div>
+    <div id='register-button-div'>
+      <form>
+        <button>Register</button>
+      </form>
+    </div>
+  </main>
+}
