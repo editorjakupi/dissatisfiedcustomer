c2966a559f5a4aeaf8576008de53341b51450dca
Marchant01 <celvin.marchant@hotmail.com>
2025-02-13 11:13:06 +0100

    Changed register page to account
    Updated the register page to account page with new input boxes.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router'
+import AccountInformation from './account.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-
+    <App/>
   </StrictMode>,
 )
+
+function App()
+{
+  return <BrowserRouter>
+  <Routes>
+    <Route path='account' element={ <AccountInformation/> } />
+  </Routes>
+  </BrowserRouter>
+}
