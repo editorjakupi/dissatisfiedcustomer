2e3d69afd5fe13b5f41176747366cac4eee4b8f5
datorn <MacbookPG@>
2025-02-18 14:35:47 +0100

    Merge main

--- server/Program.cs
+++ server/Program.cs
@@ -13,16 +13,14 @@ app.MapGet("/", () => "Hello World!");
 app.MapGet("api/login/{id}", (int id) => LoginRoute.GetUser(id, db));
 app.MapGet("/api/users", UserRoutes.GetUsers);
 app.MapGet("/api/tickets", TicketRoutes.GetTickets);
-app.MapGet("/api/opentickets", TicketRoutes.GetOpenTickets);
 app.MapPost("/api/users", UserRoutes.PostUser);
 app.MapDelete("/api/users/{id}", UserRoutes.DeleteUser);
 
-app.MapGet("/api/products/{company_id}", (int company_id) => ProductRoute.GetProducts(company_id, db));
 app.MapPost("/api/products", ProductRoute.PostProduct);
 app.MapDelete("/api/products/{id}", ProductRoute.DeleteProduct);
 app.MapPut("/api/products/{id}", ProductRoute.UpdateProduct);
 
-app.MapGet("/api/employees/{userId}",(int userId) => EmployeeRoute.GetEmployees(userId, db));
+app.MapGet("/api/employees/{userId}", (int userId) => EmployeeRoute.GetEmployees(userId, db));
 
 app.MapPost("/api/login", LoginRoute.LoginUser);
 
@@ -38,11 +36,7 @@ app.MapGet("/api/ticketform", (int ticketId) => TicketFormRoutes.GetTicketForm(t
 
 // Category api:s
 app.MapGet("/api/categories", CategoryRoutes.GetCategories);
-<<<<<<< HEAD
-=======
-
 // Lägg till den nya GET-endpointen för att visa ärendedetaljer och statusar
 app.MapGet("/user/{id}/cases/{caseId}", (int id, int caseId, NpgsqlDataSource db) => CaseRoutes.GetCaseDetails(id, caseId, db));
->>>>>>> main
 
 app.Run();
