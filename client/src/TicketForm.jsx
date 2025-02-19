0fa30aed1d23a0f218a6908e718f2620fb25c759
villiamxd <villiam.strandh45@gmail.com>
2025-02-19 16:11:03 +0100

    Fixed TicketForm GET and TicketForm.jsx
    Fixed Ticketform GET request to retrieve all needed information
    And TicketForm.jsx mostly done.

--- client/src/TicketForm.jsx
+++ client/src/TicketForm.jsx
@@ -3,30 +3,61 @@ import { createRoot } from 'react-dom/client'
 import { useParams } from 'react-router'
 import './TicketForm.css'
 
+
 export function TicketForm() {
   const params = useParams();
+  
   const [ticketform, setTicketform] = useState([]);
 
-
   useEffect(() => {
-    fetch("/api/ticketform/" + params.caseNr)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("Fetched Ticket Message:", data)
-        setTicketform(data);
-      })
-      .catch((error) => console.error("Error fetching ticket message:", error))
+    fetch("/api/ticketform?caseNumber=" + params.caseNr)
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Fetched Ticket Message:", data)
+          setTicketform(data);
+        })
+        .catch((error) => console.error("Error fetching ticket message:", error))
   }, []);
 
   return <main>
     <ul>
-      <div><h3>Title</h3><input class="title" placeholder={ticketform.title} /></div>
-      <div><h3>Email</h3><select class="product select">
-        <option>LMAO</option>
-        <option>XD</option>
+      <div><h1>{ticketform.title}</h1></div>
+      <div><h3>Category</h3><select class="category" defaultValue={null}>
+      <option key={null} value={null} hidden>Choose Category</option>
+        {ticketform.categories?.map(category => 
+          <option key={category.id} value={category.id}>{category.name}</option>)}
       </select></div>
-      <div><h3>Message</h3><textarea class="message" placeholder='Write your problem in detail here' /></div>
-      <div><button class="cancel button">Cancel</button><button class="save button" onClick="">Save</button></div>
+      <div><h3>Product/Service</h3><select class="product" defaultValue={null}>
+      <option key={null} value={null} hidden>Choose Product/Service</option>
+        {ticketform.company_products?.map(product => 
+          <option key={product.id} value={product.id}>{product.name}</option>)}
+      </select></div>
+      <div><h3>Message</h3><textarea class="message" placeholder='Write your problem in detail here' defaultValue={ticketform.description} /></div>
+      <div><button class="cancel button">Cancel</button><button class="save button" onClick={() => saveTicketForm(ticketform.ticket_id)}>Save</button></div>
     </ul>
   </main>
+}
+
+function saveTicketForm(ticketId){
+  console.log(JSON.stringify({
+    "TicketId": ticketId,
+    "ProductId": document.querySelector(".product").value,
+    "CategoryId": document.querySelector(".category").value,
+    "Content": document.querySelector(".message").value
+  }));
+  fetch("/api/ticketform", {
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    method: "POST",
+    body: JSON.stringify({
+      "TicketId": ticketId,
+      "ProductId": document.querySelector(".product").value,
+      "CategoryId": document.querySelector(".category").value,
+      "Content": document.querySelector(".message").value
+    })
+  })
+    .then(response => response.json())  // Parsa JSON responsen från backenden
+    .then(data => console.log(data));     // Logga responsen
 }
--- server/Routes/TicketFormRoutes.cs
+++ server/Routes/TicketFormRoutes.cs
@@ -2,6 +2,8 @@ using Npgsql;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Org.BouncyCastle.Cms;
 using Microsoft.Extensions.ObjectPool;
+using server.Records;
+using static server.CategoryRoutes;
 
 namespace server;
 
@@ -12,7 +14,7 @@ public class TicketFormRoutes
 
     public static async Task<Results<Created, BadRequest<string>>> PostTicketForm(TicketFormDTO ticketform, NpgsqlDataSource db)
     {
-        using var command = db.CreateCommand("UPDATE tickets SET product_id = @product_id, category_id = @category_id, description = @description WHERE id = @id");
+        await using var command = db.CreateCommand("UPDATE tickets SET product_id = @product_id, category_id = @category_id, description = @description WHERE id = @id");
         command.Parameters.AddWithValue("@product_id", ticketform.ProductId);
         command.Parameters.AddWithValue("@category_id", ticketform.CategoryId);
         command.Parameters.AddWithValue("@description", ticketform.Content);
@@ -37,9 +39,14 @@ public class TicketFormRoutes
         await using var reader = await command.ExecuteReaderAsync();
         while (await reader.ReadAsync())
         {
-            result.Add(new TicketForm(reader.GetInt32(0), reader.GetInt32(1), reader.GetString(2), reader.GetString(3)));
+            result.Add(new TicketForm(
+                reader.GetInt32(0), 
+                reader.GetInt32(1), 
+                reader.GetString(2), 
+                reader.GetString(3), 
+                ProductRoute.GetProducts(reader.GetInt32(1), db).Result, 
+                CategoryRoutes.GetCategories(db).Result));
         }
-
         return result[0];
     }
 
@@ -47,6 +54,10 @@ public class TicketFormRoutes
         int ticket_id,
         int company_id,
         string title,
-        string description
+        string description,
+        List<Products> company_products,
+        List<Category> categories
         );
+
+    
 }
