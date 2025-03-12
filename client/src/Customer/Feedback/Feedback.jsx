def5acb8073b92804807cd618ee317581b6fb42d
Villiam <37636021+villiam-xd@users.noreply.github.com>
2025-03-12 09:34:41 +0100

    Clickable stars to choose rating

--- client/src/Customer/Feedback/Feedback.jsx
+++ client/src/Customer/Feedback/Feedback.jsx
@@ -1,6 +1,7 @@
 import { StrictMode, useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import { useParams } from 'react-router';
+import '../../main.css';
 
 export function Feedback() {
 
@@ -10,23 +11,19 @@ export function Feedback() {
     const ratings = [];
     for (let index = 1; index <= 5; index++) {
       if (index <= rating) {
-        ratings.push(<div>★</div>);
+        ratings.push(<div className="star selected" id={index} onClick={(e) => setRating(e.target.id)}>★</div>);
       } else {
-        ratings.push(<div>☆</div>);
+        ratings.push(<div className="star" id={index} onClick={(e) => setRating(e.target.id)}>☆</div>);
       }
     }
     return ratings;
   }
 
 
-  return <div>
+  return <div id='feedbackWindow'>
     <ul className='rating'>
       {GetRating()}
     </ul>
-    <div>
-      <button onClick={() => setRating(rating + 1)}>+</button>
-      <button onClick={() => setRating(rating - 1)}>-</button>
-    </div>
     <textarea></textarea>
     <div><button class="cancel button">Cancel</button><button class="send button" onClick={() => xdxdxd}>Send</button></div>
   </div>
